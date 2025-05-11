8a056566a407f08f68e30dc1697496ee66ee338e
Michael Holmes <littleholmes2019@gmail.com>
2025-05-10 18:40:06 -0600

    Completed Week01 Exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage?");
+        Console.WriteLine();
+        string grade = Console.ReadLine();
+        int percentage = int.Parse(grade);
+
+        int secondDigit = percentage % 10;
+
+        string letter = "";
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else if (percentage < 60)
+        {
+            letter = "F";
+        }
+
+        string sign = "";
+        if (secondDigit >= 7)
+        {
+            sign = "+";
+        } 
+        else if (secondDigit <= 3)
+        {
+            sign = "-";
+        }
+
+
+        Console.WriteLine($"Your grade is {letter}{sign}");
+        if (percentage < 70)
+        {
+            Console.WriteLine("Regretably, you have failed the class.");
+        }
+        else 
+        {
+            Console.WriteLine("Congratulations! You have passed the class!");
+        }
     }
 }
